888fd968e024e2bfbec271b73c90efdb50f35f4c
vhLong1083 <vuhoanglonghd2003@gmail.com>
2023-10-14 23:11:04 +0700

    LoginSignUpResetPassword

--- fermovie/src/App.js
+++ fermovie/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login"
+import Signup from "./components/Signup";
+import ResetPassword from "./components/ResetPassword";
+import ResetPassword2 from "./components/ResetPassword2";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/resetpassword2" element={<ResetPassword2 />} />
+      </Routes>
+  </Router>
   );
 }
 
