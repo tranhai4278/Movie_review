bb4bf5b31417b34cb89fc2da147052a440374aac
maidung1602 <maidung1602@gmail.com>
2023-11-04 02:51:11 +0700

    merge longvh

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -65,13 +65,6 @@ export default function Filter() {
 						style={{ width: '100%', border: 'none', overflow: 'hidden' }}
 					></iframe>
 				</div>
-				<div className="sb-twitter sb-it">
-					<h4 className="sb-title">Tweet to us</h4>
-					<div className="slick-tw">
-						<div className="tweet item" id=""></div>
-						<div className="tweet item" id=""></div>
-					</div>
-				</div>
 			</div>
 		</div>
 	);
