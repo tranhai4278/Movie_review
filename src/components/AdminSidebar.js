e472ac110e35d855ba2c1e8c8c98b2e3147aa016
quyquoc <annthe170622@fpt.edu.vn>
2023-11-08 14:52:01 +0700

    [QUOCPQ] admin movie

--- src/components/AdminSidebar.js
+++ src/components/AdminSidebar.js
@@ -4,32 +4,74 @@ import { Link } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function AdminSidebar() {
-    return (
-        
-        <div className="left_col scroll-view" style={{position: 'fixed', height: "100vh"}}>
-          <Navbar.Brand style={{width: '230px'}}>
-            <Link to="/" style={{textDecoration: 'none'}}>
-              <img className="logo" src="/images/logo1.png" alt="Logo" width="160" height="58" />
-            </Link>
-          </Navbar.Brand>
+  return (
+    <div
+      className="left_col scroll-view"
+      style={{ position: "fixed", height: "100vh" }}
+    >
+      <Navbar.Brand style={{ width: "230px" }}>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <img
+            className="logo"
+            src="/images/logo1.png"
+            alt="Logo"
+            width="160"
+            height="58"
+          />
+        </Link>
+      </Navbar.Brand>
 
-          <div className="clearfix"></div>
+      <div className="clearfix"></div>
 
-          <br />
+      <br />
 
-          <div id="sidebar-menu" className="main_menu_side hidden-print main_menu" style={{marginTop: '40px'}}>
-            <div className="menu_section">
-              <ul className="nav side-menu">
-                <li><Link><i className="fa fa-desktop"></i> Movie <span className="fa fa-chevron-right"></span></Link></li>
-                <li><Link><i className="fa fa-edit"></i> Genre <span className="fa fa-chevron-right"></span></Link></li>
-                <li><Link><i className="fa fa-users-rectangle"></i> Casts <span className="fa fa-chevron-right"></span></Link></li>
-                <li><Link><i className="fa fa-user"></i> Users <span className="fa fa-chevron-right"></span></Link></li>
-                <li><Link><i className="fa fa-clipboard"></i> Report <span className="fa fa-chevron-right"></span></Link></li>
-                {/* <li><Link><i className="fa fa-bar-chart-o"></i> Report <span ></span></Link></li> */}
-              </ul>
-            </div>
-          </div>
+      <div
+        id="sidebar-menu"
+        className="main_menu_side hidden-print main_menu"
+        style={{ marginTop: "40px" }}
+      >
+        <div className="menu_section">
+          <ul className="nav side-menu">
+            <li>
+              <Link to={"/admin/movie-list"}>
+                <i className="fa fa-desktop"></i> Movie{" "}
+                <span className="fa fa-chevron-right"></span>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/admin/add/movie"}>
+                <i className="fa fa-plus"></i> Add Movie{" "}
+                <span className="fa fa-chevron-right"></span>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <i className="fa fa-edit"></i> Genre{" "}
+                <span className="fa fa-chevron-right"></span>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <i className="fa fa-users-rectangle"></i> Casts{" "}
+                <span className="fa fa-chevron-right"></span>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <i className="fa fa-user"></i> Users{" "}
+                <span className="fa fa-chevron-right"></span>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <i className="fa fa-clipboard"></i> Report{" "}
+                <span className="fa fa-chevron-right"></span>
+              </Link>
+            </li>
+            {/* <li><Link><i className="fa fa-bar-chart-o"></i> Report <span ></span></Link></li> */}
+          </ul>
         </div>
-   
-    );
+      </div>
+    </div>
+  );
 }
